025842e2f3c846d82a1257f207f3f5877c077195
СмолинИА <ivan.smolin.78@mail.ru>
2022-10-03 18:07:18 +0300

    first commit

--- app/src/main/java/com/example/proektmamsheva/MainActivity.java
+++ app/src/main/java/com/example/proektmamsheva/MainActivity.java
@@ -7,14 +7,62 @@ import android.database.Cursor;
 
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Connection connect;
+    String ConnectionResult="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void GetTextFromSQL(View v)
+    {
+        TextView tx1 = (TextView) findViewById(R.id.etName);
+        TextView tx2 = (TextView) findViewById(R.id.etEmail);
+
+        try {
+            ConnectionClass connectionClass = new ConnectionClass();
+            connect =connectionClass.connectionClass();
+            if(connect!=null)
+            {
+                String query = "Select * from Table_1";
+                Statement st = connect.createStatement();
+                ResultSet rs = st.executeQuery(query);
+
+                while (rs.next())
+                {
+                    tx1.setText(rs.getString(1));
+                    tx2.setText(rs.getString(2));
+                }
+
+            }
+            else
+            {
+                ConnectionResult = "Check Connection";
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/proektmamsheva/SqlConnect.java
+++ app/src/main/java/com/example/proektmamsheva/SqlConnect.java
@@ -1,28 +1,47 @@
 package com.example.proektmamsheva;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SqlConnect {
+class ConnectionClass{
+    Connection con;
+    String uname, pass, ip,port,database;
+    @SuppressLint("NewApi")
 
-    public static void main(String[] args) {
-        String connectionUrl =
-                "jdbc:sqlserver://ngknn.ru.database.windows.net:1433;"
-                        + "database=AnJava;"
-                        + "user=33П;"
-                        + "password=12357;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
+    public Connection connectionClass()
+    {
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+        ip = "172.1.1.0";
+        database="Registr";
+        uname="33П";
+        pass="12357";
+        port="1433";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try
+        {
+
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://"+ ip + ":" + port + ";" + "databasename=" + database + ";user=" + uname + ";password="+ pass + ";";
+            connection= DriverManager.getConnection(ConnectionURL);
 
         }
-
-        catch (SQLException e) {
-            e.printStackTrace();
+        catch (Exception ex)
+        {
+            Log.e("Error", ex.getMessage());
         }
+
+        return connection;
     }
+
 
 }
